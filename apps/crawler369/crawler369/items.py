f2f324ced9623f0ee9365a19a6598f1ddd121283
ski <none@none>
2011-03-26 04:31:39 +0200

    Delfi scrapper start over. Model restructured

--- apps/crawler369/crawler369/items.py
+++ apps/crawler369/crawler369/items.py
@@ -4,18 +4,8 @@
 # http://doc.scrapy.org/topics/items.html
 
 from scrapy.item import Item, Field
-from scrapy.contrib_exp.djangoitem import DjangoItem
-from tsd.core.models import Documents
 
-class DelfiLtItem(DjangoItem):
+class CrawlerItem(Item):
     # define the fields for your item here like:
     # name = Field()
-    django_model = Documents
-
-class Dmoz(Item):
-    title = Field()
-    link = Field()
-    desc = Field()
-    
-    
-    
+    pass
---
+++ apps/crawler369/crawler369/spiders/delfi_lt.py
@@ -0,0 +1,12 @@
+from scrapy.spider import BaseSpider
+
+class DelfiLtComments(BaseSpider):
+    name = "delfi_lt_comments"
+    allowed_domains = ["delfi.lt"]
+    start_urls = [
+        "http://www.delfi.lt",
+    ]
+
+    def parse(self, response):
+        filename = response.url.split("/")[-2]
+        open(filename, 'wb').write(response.body)
