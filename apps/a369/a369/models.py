41892557304d0db401a9cc6d7d2b865f51c05051
ski <none@none>
2011-03-27 12:25:13 +0300

    Increased title len

--- apps/a369/a369/models.py
+++ apps/a369/a369/models.py
@@ -45,7 +45,7 @@ class CommentItem(BaseItem):
     content = models.TextField()
     content_length = models.IntegerField(default=0, null=True)
     content_word_count = models.IntegerField(default=0, null=True)
-    subject_title = models.CharField(max_length=255, null=True)
+    subject_title = models.CharField(max_length=555, null=True)
     subject_type = models.CharField(max_length=255, choices=SUBJECT_CHOICES)
     subject_id = models.CharField(max_length=255)
 
