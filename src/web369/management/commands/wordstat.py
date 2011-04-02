55e3103cfa8530c2a19ac861e5cbfecb446f2957
ski <none@none>
2011-04-02 18:48:01 +0300

    Renamed kewords command to wordstat and added info about current statistic.
    --HG--
    rename : src/web369/management/commands/keywords.py => src/web369/management/commands/wordstat.py

--- src/web369/management/commands/wordstat.py
+++ src/web369/management/commands/wordstat.py
@@ -7,20 +7,20 @@ from optparse import make_option
 
 from django.core.management.base import BaseCommand
 
-from web369.models import BaseWord
+from web369.models import BaseWord, Word, ScrappedDocument
 
 
 class Command(BaseCommand):
     args = ''
-    help = 'Display highest ranked keywords'
+    help = 'Display statistic, highest ranked keywords, recount words'
 
     option_list = BaseCommand.option_list + (
 
-        make_option('--limit',
+        make_option('--limit', '-n',
             action='store',
             dest='limit',
             default=10,
-            help='Limit to'),
+            help='Number of most popular words to show'),
 
         make_option('--recount',
             action='store_true',
@@ -36,6 +36,10 @@ class Command(BaseCommand):
         words = BaseWord.objects.filter(stop_word=False) \
                                 .with_count() \
                                 .order_by('-count')[:limit]
+        print "\nStats:\n"
+        print "  Base-words: %d" % BaseWord.objects.count()
+        print "  Words: %d" % Word.objects.count()
+        print "  Documents: %d" % ScrappedDocument.objects.count()
         print "\nMost popular words:\n"
         for word in words:
             print u"%5d | %s" % (word.count, word)
