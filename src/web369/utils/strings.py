48d90002ae10d7d2c6da8c2d6f245f1a9bbfd1c3
ski <none@none>
2011-04-04 13:32:00 +0300

    Restored count_words method. It was removed accidentely.

--- src/web369/utils/strings.py
+++ src/web369/utils/strings.py
@@ -23,6 +23,22 @@ def split_words(text):
     return re.sub('\W+', ' ', text).split(' ')
 
 
+def count_words(text, match=None):
+    """
+    Returns a list of (word, value) pairs.
+
+    >>> count_words("a a b c")
+    [('a', 2), ('c', 1), ('b', 1)]
+    >>> count_words("a a b c", match="[ab]+")
+    [('a', 2), ('b', 1)]
+    """
+    word_list = split_words(text)
+    unique_split_words = set(word_list)
+    return [(word, word_list.count(word))
+            for word in unique_split_words
+            if match == None or re.match(match, word)]
+
+
 def highlight_query(text, query, formatting="<strong>%s</strong>"):
     u"""
     >>> text = u"Geri vyrai geroi girioi Gerą girą gerai gėrė"
